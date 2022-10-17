564f5ceb772a233b09e43a9c19dbd0cc2dedd6a8
Voragun <voragun2543@gmail.com>
2022-10-17 21:21:59 +0700

    partition sort version2

--- code/Start.java
+++ code/Start.java
@@ -1,16 +1,34 @@
 class Start {
     public static void main(String[] args) {
-        int[] a = { 71, 80, 72, 38, 45, 63, 12,80, 99 };
+        // int[] a = { 71, 80, 72, 38, 45, 63, 12,80, 99 };
+        
+        int[] a = new int[100000];
+        for(int i = 0; i < a.length; i++) a[i] = (int)(Math.random() * 1000000);
+        
         Engine e = new Engine();
-        e.partition(a, 0, a.length - 1);
-        System.out.println("Before (Partition already) : ");
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(" " + a[i]);
+        e.sort(a);
+        boolean flag = true;
+        for(int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i+1]) {
+                flag = false;
+            }
         }
+        System.out.println(flag);
     }
 }
 
 class Engine {
+    
+    void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+    
+    void sort(int[] a, int left, int right) {
+        if (left >= right) return;   // only one element
+        int p = partition(a, left, right);
+        sort(a, left, p - 1);
+        sort(a, p + 1, right);
+    }
     
     int partition(int[] a, int left, int right) {
         // first version is use the left-most element be the pivot
