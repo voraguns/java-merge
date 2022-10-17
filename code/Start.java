cbd07d7af94f02f0d293fbacc5eae53a019e9b5f
Voragun <voragun2543@gmail.com>
2022-10-17 21:12:56 +0700

    partition sort

--- code/Start.java
+++ code/Start.java
@@ -1,41 +1,35 @@
 class Start {
     public static void main(String[] args) {
-        int[] a = { 8, 5, 4, 7, 2, 3, 6, 4 };
-        
-        System.out.println("Original Version :");
-        for(int i = 0; i < a.length; i++) {
-            System.out.print(" " + a[i]);
-        }
-        System.out.println("");
-        
-        System.out.println("Print all even : ");
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] % 2 == 0) {
-                System.out.print(" " + a[i]);
-            }
-        }
-        
-        System.out.println("");
-        
-        System.out.println("Reverse all even values only : ");
-        int left = 0;
-        int right = a.length - 1;
-        while(left <= right) {
-            if (a[left] % 2 == 0 && a[right] % 2 == 0) {
-                int x = a[left];
-                int y = a[right];
-                a[left] = y;
-                a[right] = x;
-                left++; 
-                right--;
-                continue;
-            }
-            if (a[left] % 2 != 0 && a[right] % 2 == 0) { left++; continue; }
-            if (a[left] % 2 == 0 && a[right] % 2 != 0) { right--; continue; }
-            if (a[left] % 2 != 0 && a[right] % 2 != 0) { left++; right--; continue; }
-        }
+        int[] a = { 71, 80, 72, 38, 45, 63, 12,80, 99 };
+        Engine e = new Engine();
+        e.partition(a, 0, a.length - 1);
+        System.out.println("Before (Partition already) : ");
         for (int i = 0; i < a.length; i++) {
             System.out.print(" " + a[i]);
         }
+    }
+}
+
+class Engine {
+    
+    int partition(int[] a, int left, int right) {
+        // first version is use the left-most element be the pivot
+        // the better version is to random, to prevent the worst-case 
+        int pivot = a[left];
+        int i = left + 1;      // skip the pivot
+        int j = right;
+        while(i <= j) {
+            while (i <= j && a[i] <= pivot) i++;
+            while (i <= j && a[j] >= pivot) j--;
+            if (i <= j) {
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }          
+        }
+        int t = a[left];
+        a[left] = a[j];
+        a[j] = t;
+        return j;
     }
 }
