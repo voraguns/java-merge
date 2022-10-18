ca2e547306f510bca195de9c3627c3c411f2476a
Voragun <voragun2543@gmail.com>
2022-10-18 20:35:46 +0700

    Dijkstra Partition

--- code/Start.java
+++ code/Start.java
@@ -1,14 +1,14 @@
 class Start {
     public static void main(String[] args) {
-        // int[] a = { 71, 80, 72, 38, 45, 63, 12,80, 99 };
-        
-        int[] a = new int[100000];
-        for(int i = 0; i < a.length; i++) a[i] = (int)(Math.random() * 1000000);
+        // int[] a = { 71, 80, 72, 38, 45, 63, 12, 80, 99 };
+        int[] a = new int[10000000];
+        for (int i = 0; i < a.length; i++) 
+            a[i] = (int)(Math.random() * 1000);
         
         Engine e = new Engine();
         e.sort(a);
         boolean flag = true;
-        for(int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < a.length - 1; i++) {
             if (a[i] > a[i+1]) {
                 flag = false;
             }
@@ -19,35 +19,47 @@ class Start {
 
 class Engine {
     
-    void sort(int[] a) {
+    void sort (int[] a) {
         sort(a, 0, a.length - 1);
     }
     
     void sort(int[] a, int left, int right) {
-        if (left >= right) return;   // only one element
-        int p = partition(a, left, right);
-        sort(a, left, p - 1);
-        sort(a, p + 1, right);
-    }
-    
-    int partition(int[] a, int left, int right) {
-        // first version is use the left-most element be the pivot
-        // the better version is to random, to prevent the worst-case 
-        int pivot = a[left];
-        int i = left + 1;      // skip the pivot
-        int j = right;
-        while(i <= j) {
-            while (i <= j && a[i] <= pivot) i++;
-            while (i <= j && a[j] >= pivot) j--;
-            if (i <= j) {
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
-            }          
+        if (left >= right) return;
+        /// pivot located on the right hand side
+        int pivot = a[right];  // random is better
+        int lower = left;
+        int upper = right - 1;
+        int i = left;
+        while (i <= upper) {
+            int compare = a[i] - pivot;
+            
+            // this element is less than pivot
+            if (compare < 0) { 
+                int t = a[lower];    // exchange a[i] and a[lower]
+                a[lower] = a[i];
+                a[i] = t;
+                i++;
+                lower++;
+            }
+            
+            // this element is equal to pivot
+            if (compare == 0) {
+                i++;
+            }
+            
+            // this element is greater than pivot
+            if (compare > 0) { 
+                int t = a[upper];     // exhange a[i] and a[upper]
+                a[upper] = a[i];
+                a[i] = t;
+                upper--;
+            }
         }
-        int t = a[left];
-        a[left] = a[j];
-        a[j] = t;
-        return j;
+        int t = a[lower];
+        a[lower] = a[right];
+        a[right] = t;
+        
+        sort(a, left, lower - 1);
+        sort(a, upper + 1, right);
     }
 }
